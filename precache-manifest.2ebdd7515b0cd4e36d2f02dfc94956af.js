70635d733fcd1de921ae78d6e56b8bf2fac44d4d
hkar19 <5562429+hkar19@users.noreply.github.com>
2020-06-11 16:02:37 +0700

    copas isi build

---
+++ precache-manifest.2ebdd7515b0cd4e36d2f02dfc94956af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31779e4780fb94893c76",
+    "url": "/static/css/main.438e6e48.chunk.css"
+  },
+  {
+    "revision": "31779e4780fb94893c76",
+    "url": "/static/js/main.31779e47.chunk.js"
+  },
+  {
+    "revision": "2e429677ab90a8f486cb",
+    "url": "/static/js/1.2e429677.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b1d53f7936c094b5d10b8abe0fbe61e",
+    "url": "/index.html"
+  }
+];
